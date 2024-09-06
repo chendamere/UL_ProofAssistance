eb39cc0cefaf18520011313ca648d331e6479a8b
Hankai Chen <87444158+chendamere@users.noreply.github.com>
2024-09-06 15:53:27 -0400

    checkpoint for fixing fixing infinite loop

--- UL_Interpreter/main.js
+++ UL_Interpreter/main.js
@@ -13,112 +13,179 @@ let axioms = LatexChapters(rf, './axiom', p)
 
 let tstatements = LatexChapters(rf, './theorems', p)
 
+//expression does not contain duplicate of beggin expressions, but it contains end expressions
 let tpexp = LatexExps(rf,'./theorems',true)
 
 var pf = new ProofAssistant(axioms, p, tpexp)
-pf.PrintAllRules()
+// pf.PrintAllRules()
 
 // console.log(pf.isRule(pf.genRule('!,#11, @, #11, #11, \n')[0]))
 // console.log(tpexp)
-let beginexp = []
-let endexp = []
-let expstack =[]
 
+var beginexp = []
+var endexp = []
 for(const r of tstatements){
     let s = pf.RuleToString(r)
 
     let el = s.split('@')[0]
     let er = s.split('@')[1]
-
-    if(!beginexp.includes(el)){
-        beginexp.push(el)
-    }
-    if(!endexp.includes(er)){
-        endexp.push(er)
-    }
+    beginexp.push(el)
+    endexp.push(er)
 }
+
 // console.log(pf.isRule(pf.genRule('!, #12 $1 $1 , #11 , #15 1 ,  @ , #13 $1 $1 #10 2 3 , #4 1 , #4 1 ,\n')[0]))
-let expi = 0
+var expstack =[]
+
+var expi = 0
 var index = 1
+var stackindex=0
 var next = []
 // let ret = pf.genRule('!,#4 1,@,#4 1,\n')[0]
 // console.log(pf.Same(ret.leftexps, ret.rightexps))
+
+// console.log(beginexp)
+console.log('-----------')
+// console.log(endexp)
+
+//prove all rules
 while(expi < beginexp.length){
     console.log('---------- proof ',expi,' begin ----------')
-    // console.log(expstack.length,expi)
+
     expstack.push([])
     let fromstack = false 
-
     let p = beginexp[expi]
     let q = endexp[expi]
-    console.log('start: ',p, 'end: ', q)
+    console.log('begin start: ',p, 'end: ', q)
     
+    //check if rule is already a rule 
+    //isRule matches code variables with operations
     let r = (pf.genRule('!'+p+'@'+q+'\n')[0])
-    // console.log(pf.RuleToString(r))
+
     if(!pf.isRule(r)){
-        //try all expression in pf
+        //first try, look for provable expressions in tpexp,
+        //e is the next rule to prove, or else is -1
+        
         let e = proveExps(tpexp, p)
         next = e[0]
         index = e[1]
-        let tempnext, tempstack
+        
+        let tempstack
         if(next == -1) {
-            [tempnext, tempstack] = provefromstack(p)
-
-            if(tempnext != -1){
+            let x = provefromstack(expi, p,stackindex)
+            if(x[0] != -1){
                 fromstack = true
-                // console.log('fromstack: ', fromstack)
-                next = tempnext
-
+                next = x[0]
+                tempstack=x[1]
                 tempstack.splice(0,1)
+                expstack[expi].push(next)
+                stackindex = x[2]
+                console.log('add to stack0: ',next)
+
+                // console.log('add to stack0: ',next)
+            }
+            else{
+                throw new Error('no matching expression in provefromstack')
             }
         }
         else{
-            // console.log('next: ', next)
 
+            //keep track of all expression that are used in a stack
             expstack[expi].push(next)
-            tpexp.splice(index,1)
+            console.log('add to stack1: ',next)
 
+            //remove the found expression in tpexp
+            tpexp.splice(index,1)
         }
 
+        //by this point we have proven 1 rule, next is either -1 or the next rule
         let npr = pf.genRule('!'+next +'@'+endexp[expi]+'\n')[0]
 
         //prove the rest
+        //if next == -1 then it will pass automatically
         while(next != -1 && !pf.Same(npr.leftexps, npr.rightexps)){
-            p = next 
-
             if(fromstack){
-                let e = proveExps(tempstack, p)
-                next = e[0]
-                index = e[1]
-                if(next!= -1){
+                let e = proveExps(tempstack, next)
+                
+                if(e[0] != -1){
+                    // tempstack=x[1]
                     tempstack.splice(0,1)
+                    next = e[0]
+                    // console.log('add to stack0: ',next)
+                }
+                else{
+                    //maybe here look for the next stack given stack index
+
+                    let x = provefromstack(expi, next, stackindex)
+                    // console.log('*&*&', x )
+
+                    if(x[0] != -1){
+                        fromstack = true
+                        next = x[0]
+                        tempstack=x[1]
+                        tempstack.splice(0,1)
+                        stackindex = x[2]
+
+                        // console.log('*&*&', tempstack)
+                    }else{
+                        throw new Error('failed in prove rest from proveExps->tempstack->provefromstack')
+                    }
                 }
             }
             else{
+                let e = proveExps(tpexp, next)
 
-                let e = proveExps(tpexp, p)
-                // console.log('nextexp: ',e[0])
-                next = e[0]
-                index = e[1]
-                if(next!= -1){
+                if(e[0] != -1){
+                    next = e[0]
+                    index = e[1]
                     tpexp.splice(index,1)
                 }
+                else if(next ==-1){
+                    //reset next and run the loop again
+                    let x = provefromstack(expi, next, stackindex)
+                    // console.log('*&*&', x )
 
+                    if(x[0] != -1){
+                        fromstack = true
+                        next = x[0]
+                        tempstack=x[1]
+                        tempstack.splice(0,1)
+                        stackindex = x[2]
+
+                        // console.log('*&*&', tempstack)
+                    }                    
+                }
+                else{
+                    throw new Error('failed in prove rest from proveExps->texp && provefromstack')
+                }
             }
 
-            expstack[expi].push(next)
             if(next == -1) {
-                console.debug('next: ', next)
+                throw new Error('debug next: ', next)
             }
-            npr = pf.genRule('!'+next +'@'+endexp[expi]+'\n')[0]
-            // console.log('next :', next)
+            else{
+                //add to the expstack
+                expstack[expi].push(next)
+                console.log('add to stack5: ',next, expi, )
+                npr = pf.genRule('!'+next +'@'+endexp[expi]+'\n')[0]
+                // console.log(pf.isRule(npr), pf.RuleToString(npr))
+                // let check = pf.Operands_normalize(pf.genRule('!'+pf.ExpToString(npr.leftexps) +'@'+endexp[expi]+'\n')[0])
+                // console.log('goal? ', pf.RuleToString(goal))
+                // console.log(pf.ExpToString(npr.leftexps),pf.ExpToString(npr.rightexps))
+
+                // if(pf.Same(npr.leftexps,npr.rightexps) && ! pf.Same(goal.leftexps, goal.rightexps)){
+                //     console.log(pf.ExpToString(npr.leftexps),pf.ExpToString(npr.rightexps))
+                //     throw new Error('stuck in infinite loop: ')
+                // }
+            }
         }
+        
     }
 
     
 
     let newrule = pf.genRule('!'+beginexp[expi]+'@'+endexp[expi]+'\n')[0]
     console.log('newrule: ', pf.RuleToString(newrule))
+    console.log('expstack[expi]:',expstack[expi])
     pf.allrules.push(newrule)
 
     console.log('--------- proof ',expi,' end ----------')
@@ -130,20 +197,17 @@ while(expi < beginexp.length){
 
 
 
-function proveExps(tpexp, p) { 
+function proveExps(exps, p) { 
     let i = 0
 
-    if(!tpexp) return [-1,-1]
-    while (tpexp[i] && tpexp[i].length !=0){
-        let l = tpexp[i]
-        // console.log(l)
-        // console.log('from proveExps?: ',p,l)
+    if(!exps) return [-1,-1]
+    while (exps[i] && exps[i].length !=0){
+        let l = exps[i]
+        console.log('from proveExps?: ',p,l)
         let t = pf.Proving(p, l)
+        console.log('t: ',t)
         if(t != -1) {
-            // console.log('here: ',t)
-
             return [t[0], i]
-            //add every exp execpt for the one just used
         }
     
         
@@ -152,71 +216,49 @@ function proveExps(tpexp, p) {
     
     return [-1,-1]
 }
-function provefromstack(p, stackindex){
+function provefromstack(end, p, stackindex){
     let i =0
     if(stackindex != 0) i = stackindex
     else i=0
-    // console.log('provefromstack:')
+    console.log('provefromstack:')
     // console.log(beginexp.length, expstack.length,expstack)
-    while ( i < beginexp.length){
+    while ( i < beginexp.length-1 && i< end){
         let l = beginexp[i]
         let fakerule = pf.genRule('!'+p+'@'+l+'\n')[0]
 
         let x = getchangeoperator(fakerule)
 
         let tstack = []
-        // console.log(expstack[i])
-        let alt = false 
-        let e 
-        let ret
-        while(true){
-            for(const l of expstack[i]){
-                let r = replaceoperator(x[0],x[1], l, alt)
-                if(r) 
-                {
-                    if(r.length==0){
-                        tstack.push(l)
-                    }
-                    tstack.push(r)
+        console.log(expstack[i])
+        
+        for(const l of expstack[i]){
+            let r = replaceoperator(x[0],x[1], l)
+            if(r) 
+            {
+                if(r.length==0){
+                    tstack.push(l)
                 }
-            }
-            // console.log('JKJK: ', tstack)
-            e= proveExps(tstack,p)
-            ret=e[0]
-            if(ret != -1 && pf.Same(e[0], endexp[i])){
-                return [ret,tstack, i]
-            }else{
-                if(!alt){
-                    alt = true
-                }
-                else{
-                    tstack = []
-                    break
-                }
+                tstack.push(r)
             }
         }
-        
+        // console.log('JKJK: ', tstack)
+        let e= proveExps(tstack,p)
+        let ret=e[0]
+        if(ret != -1){
+            return [ret,tstack, i]
+        }
 
-        // let stbe = switchtopbot(l)
-        // let flipedr = pf.genRule('!'+p+'@'+stbe+'\n')[0]
         let flipstack =[]
-        // console.log(pf.ExpToString(fakerule.rightexps), 'switched: ',pf.ExpToString(switchtopbot(fakerule.rightexps)))
-        // console.log(expstack,i)
         for(const l of expstack[i]){
             // console.log(l)
             if(l ==[]) l =''
-            
-            // console.log(stb)
-            // stb = stb.trim()
             flipstack.push(l)
         }
-        // console.log(flipstack)
         e= proveExps(flipstack,p)
         ret=e[0]
-        // console.log('finished?',p, '@' ,ret)
-        // let index=e[1]
+
         if(ret != -1){
-            return [ret,flipstack]
+            return [ret,flipstack, i]
         }
         i+=1
 
@@ -224,7 +266,7 @@ function provefromstack(p, stackindex){
     console.log('no matching begin expression')
     return [-1,-1]
 }
-function replaceoperator(src, tar, l, alt){
+function replaceoperator(src, tar, l){
     let dummystatement = pf.genRule('!'+',@'+l+'\n')[0]
     
     for (const op of dummystatement.rightexps){
@@ -242,26 +284,20 @@ function replaceoperator(src, tar, l, alt){
                 }
 
                 if(pf.binaryOperators.includes(src) && pf.unaryOperators.includes(tar)){
-                    if(alt){
-                        let tempv = op.operands[op.operands.length-1]
-                        op.operands = op.operands.slice(0,op.operands.length-2)
-                        op.operands.push(tempv)
-                    }else{
-                        op.operands = op.operands.slice(0,op.operands.length-1)
-                    }
+                    op.operands = op.operands.slice(0,op.operands.length-1)
                 }
                 else if(pf.binaryOperators.includes(tar) && pf.unaryOperators.includes(src)){
                     op.operands.push(op.operands[op.operands.length]+1)
                 }
-                
                 let ret = pf.ExpToString(dummystatement.rightexps)
-                // console.log('dummy: ',ret)
+                console.log('dummy: ',ret)
                 return ret
             
             }
         }
     }
-    return []
+    
+    return l
 }
 
 function getchangeoperator(fakerule) {
